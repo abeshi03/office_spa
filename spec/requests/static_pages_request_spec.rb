79624894d679e2313d9f99bb602309f0c6cc0576
kohei abe <k_913411@yahoo.co.jp>
2020-10-03 23:03:23 +0900

    add to application_helper_spec is title_test

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
     it "returns http success" do
-      get "/static_pages/home"
+      get root_path
       expect(response).to have_http_status(:success)
     end
   end
