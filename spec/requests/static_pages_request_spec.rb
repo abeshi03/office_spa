e0a05b94ac41aa36db3d242f288a3c871ecbef3f
kohei abe <k_913411@yahoo.co.jp>
2020-10-05 13:23:46 +0900

    add to static_pages request_spec

--- spec/requests/static_pages_request_spec.rb
+++ spec/requests/static_pages_request_spec.rb
@@ -2,9 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   describe "GET /home" do
-    it "returns http success" do
+    before do
       get root_path
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+
+    it "there should be the specified character" do
+      expect(response.body).to include "ログイン"
+      expect(response.body).to include "新規登録"
+      expect(response.body).to include "ゲストログイン"
     end
   end
 end
