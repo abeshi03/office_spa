d2627d01284a02cedf4f340ccc30ecc05d277dc6
kohei abe <k_913411@yahoo.co.jp>
2020-10-06 16:42:59 +0900

    add to users_request_spec

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -1,12 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-
   describe "GET /new" do
+    before do
+      get signup_path
+    end
+
     it "returns http success" do
-      get "/users/new"
       expect(response).to have_http_status(:success)
     end
-  end
 
+    it "there is a new registration screen" do
+      expect(response.body).to include "新規登録"
+    end
+
+    it "label display test" do
+      expect(response.body).to include "フルネーム"
+      expect(response.body).to include "メールアドレス"
+      expect(response.body).to include "パスワード(6文字以上)"
+      expect(response.body).to include "パスワード(確認用)"
+      expect(response.body).to include "作成する"
+    end
+  end
 end
