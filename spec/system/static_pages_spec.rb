174f29d562a522b536f57f8cf6d6cafe9f2cb389
kohei abe <k_913411@yahoo.co.jp>
2020-10-06 16:17:40 +0900

    add to static_pages system_spec link_test

--- spec/system/static_pages_spec.rb
+++ spec/system/static_pages_spec.rb
@@ -1,4 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :system do
+  describe "StaticPages/home" do
+    before do
+      visit root_path
+    end
+
+    it "signup link test" do
+      click_on "新規登録"
+      expect(current_path).to eq signup_path
+    end
+
+    it "click_test is logo" do
+      find(".logo").click
+      expect(current_path).to eq root_path
+    end
+  end
 end
