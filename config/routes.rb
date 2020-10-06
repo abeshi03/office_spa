edb3fe2edcbae94d7f94d0745431cdad19379eeb
kohei abe <k_913411@yahoo.co.jp>
2020-10-06 11:37:36 +0900

    create by sugnup_page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
+  get '/signup', to: 'users#new'
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
