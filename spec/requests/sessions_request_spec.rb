1e97a0ca3ed0aa9e2dcb9a1c4859ce1a467726e4
kohei abe <k_913411@yahoo.co.jp>
2020-10-06 19:31:38 +0900

    edit login_page layout

--- spec/requests/sessions_request_spec.rb
+++ spec/requests/sessions_request_spec.rb
@@ -2,9 +2,23 @@ require 'rails_helper'
 
 RSpec.describe "Sessions", type: :request do
   describe "GET /new" do
-    it "returns http success" do
+    before do
       get login_path
+    end
+
+    it "returns http success" do
       expect(response).to have_http_status(:success)
+    end
+
+    it "login string test" do
+      expect(response.body).to include "ログイン"
+    end
+
+    it "there should be a label and a link" do
+      expect(response.body).to include "メールアドレス"
+      expect(response.body).to include "パスワード"
+      expect(response.body).to include "新規登録はこちら"
+      expect(response.body).to include "ゲストでログインの方はこちら"
     end
   end
 end
