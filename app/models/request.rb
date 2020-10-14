7d184d81125219088bf737d7a42557048437d56b
kohei abe <k_913411@yahoo.co.jp>
2020-10-14 16:27:18 +0900

    add to request content validation test

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,4 +1,5 @@
 class Request < ApplicationRecord
   belongs_to :user
   validates :user_id, presence: true
+  validates :content, presence: true, length: { maximum: 200 }
 end
--- spec/models/request_spec.rb
+++ spec/models/request_spec.rb
@@ -11,4 +11,24 @@ RSpec.describe Request, type: :model do
     request.user = nil
     expect(request).not_to be_valid
   end
+  
+  it "content present" do
+    request.content = ""
+    expect(request).not_to be_valid
+  end
+
+  it "content nil" do
+    request.content = nil
+    expect(request).not_to be_valid
+  end
+
+  it "content characters is 100" do
+    request.content = "a" * 100
+    expect(request).to be_valid
+  end
+
+  it "content characters is 201" do
+    request.content = "a" * 201
+    expect(request).not_to be_valid
+  end
 end
