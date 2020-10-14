147416b393551bc86914a6e9d07d4ef14ae56034
kohei abe <k_913411@yahoo.co.jp>
2020-10-14 22:12:45 +0900

    new_request_path layout set

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   post '/gest_login', to: 'gest_login#new_gest'
   resources :users
-  resources :requests, only: [:index]
+  resources :requests, only: [:index, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
