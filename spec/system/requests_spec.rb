6825625783225c3cf9c14fd567ce7efb690722e8
kohei abe <k_913411@yahoo.co.jp>
2020-10-16 11:44:34 +0900

    edit system_spec request_destroy_test

--- spec/system/requests_spec.rb
+++ spec/system/requests_spec.rb
@@ -32,25 +32,47 @@ RSpec.describe "Requests", type: :system do
       click_on "要望一覧はこちら"
       expect(current_path).to eq requests_path
     end
+  end
 
-    describe "request/index" do
-      let!(:second_user) { create :second_user }
-      let!(:request) { create :request }
+  describe "request/index" do
+    let!(:other) { create :other }
+    let!(:request) { create :request }
 
-      before do
-        log_in_as second_user
-        visit requests_path
-      end
+    before do
+      log_in_as other
+      visit requests_path
+    end
 
-      it "new_request_path link test" do
-        click_on "要望を投稿する"
-        expect(current_path).to eq new_request_path
-      end
+    it "new_request_path link test" do
+      click_on "要望を投稿する"
+      expect(current_path).to eq new_request_path
+    end
 
-      it "request_show link test" do
-        first(".comment-icon").click
-        expect(current_path).to eq requests_path
-      end
+    it "request_show link test" do
+      first(".comment-icon").click
+      expect(current_path).to eq requests_path
+    end
+  end
+
+  describe "request/show/destroy" do
+    let!(:second_user) { create :second_user }
+    let!(:request) { create :request }
+
+    before do
+      log_in_as second_user
+      visit request_path(request.id)
+    end
+
+    it "should be request name" do
+      expect(page).to have_content request.user.name
+    end
+
+    it "should be request content" do
+      expect(page).to have_content request.content
+    end
+
+    it "not request destroy" do
+      expect(page).not_to have_content "要望を削除する"
     end
   end
 end
