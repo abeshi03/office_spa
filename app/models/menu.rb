8193596ad4d91a6d26ff7e0ff6f67d47f9b90bec
kohei abe <k_913411@yahoo.co.jp>
2020-10-19 01:26:23 +0900

    image riseze

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -4,4 +4,10 @@ class Menu < ApplicationRecord
   validates  :name,        presence: true
   validates  :category,    presence: true
   validates  :description, presence: true, length: { maximum: 100 }
+  validates :image, content_type: { in: %w[image/jpeg image/gif image/png], message: "must be a valid image format" },
+                            size: { less_than: 5.megabytes, message: "should be less than 5MB" }
+
+  def display_image
+    image.variant(resize_to_limit: [200, 200])
+  end
 end
