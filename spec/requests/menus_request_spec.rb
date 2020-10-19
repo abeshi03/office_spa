85df3aa4592caee355c6fc30d82d1b6293ec0fb1
kohei abe <k_913411@yahoo.co.jp>
2020-10-19 22:24:02 +0900

    add to menus_index request_spec

--- spec/requests/menus_request_spec.rb
+++ spec/requests/menus_request_spec.rb
@@ -36,4 +36,45 @@ RSpec.describe "Menus", type: :request do
       end
     end
   end
+
+  describe "menu/index" do
+    let!(:user) { create :user }
+    let!(:admin) { create :admin }
+    let!(:menu) { create :menu }
+
+    context "user" do
+      before do
+        sign_in_as user
+        get menus_path
+      end
+
+      it "index page response" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "menus page response body" do
+        expect(response.body).to include "メニュー一覧"
+        expect(response.body).to include menu.name
+        expect(response.body).to include menu.description
+        expect(response.body).to include menu.category
+        expect(response.body).to include "予約する"
+      end
+    end
+
+    context "admin" do
+      before do
+        sign_in_as admin
+        get menus_path
+      end
+
+      it "admin response success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "admin only link" do
+        expect(response.body).to include "メニューを削除する"
+        expect(response.body).to include "メニューを追加する"
+      end
+    end
+  end
 end
