7bfb55e97388a57a86960aa69fd3ccfd6ce1be77
kohei abe <k_913411@yahoo.co.jp>
2020-10-21 01:56:01 +0900

    create review form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,11 @@
 class ReviewsController < ApplicationController
   def index
+    @menu = Menu.find(params[:menu_id])
+    @reviews = @menu.reviews
+  end
+
+  def show
+    @menu = Review.find(params[:id])
+    @review = Review.new
   end
 end
