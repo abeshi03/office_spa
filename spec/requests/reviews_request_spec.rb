bddd3cffdeb25c8c50429c8fed5f2ea080c10052
kohei abe <k_913411@yahoo.co.jp>
2020-10-21 17:35:25 +0900

    add to reviews_request_spec

--- spec/requests/reviews_request_spec.rb
+++ spec/requests/reviews_request_spec.rb
@@ -2,7 +2,26 @@ require 'rails_helper'
 
 RSpec.describe "Reviews", type: :request do
 
-  describe "GET /index" do
+  describe "menus/show" do
+    let!(:user)   { create :user }
+    let!(:menu)   { create :menu }
+    let!(:review) { create :review }
+
+    before do
+      sign_in_as user
+      get menu_path(menu.id)
+    end
+
+    it "menu show response success" do
+      expect(response).to have_http_status(:success)
+    end
+
+    it "response body menu show" do
+      expect(response.body).to include "口コミ"
+      expect(response.body).to include menu.name
+      expect(response.body).to include "口コミを確認する"
+      expect(response.body).to include "口コミを投稿する"
+    end
   end
 
 end
