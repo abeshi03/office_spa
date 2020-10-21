25b30586b63ee85b2ffa504a3e91f9a49ff187c0
kohei abe <k_913411@yahoo.co.jp>
2020-10-21 15:50:53 +0900

    create reviews.js

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,23 @@
 class ReviewsController < ApplicationController
+  before_action :logged_in_user, only: [:create]
   def index
     @menu = Menu.find(params[:menu_id])
     @reviews = @menu.reviews
   end
 
-  def show
-    @menu = Review.find(params[:id])
-    @review = Review.new
+  def create
+    @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    if @review.save
+      redirect_to menu_reviews_path(@review.menu)
+    else
+      @menu = Menu.find(params[:menu_id])
+      render "menus/show"
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:menu_id, :score, :content)
   end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
   belongs_to :menu
+  validates  :score,   presence: true
+  validates  :content, presence: true
 end
