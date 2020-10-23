353630d95fe441f2d1e11fc244136876bf6723b0
kohei abe <k_913411@yahoo.co.jp>
2020-10-23 22:45:21 +0900

    validate reservation start_time

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -3,6 +3,7 @@ class Reservation < ApplicationRecord
   validate :date_before_start
   validate :start_time_not_sunday
   validate :start_time_not_saturday
+  validate :time_only_1
 
 
   def date_before_start
@@ -16,5 +17,27 @@ class Reservation < ApplicationRecord
 
   def start_time_not_saturday
     errors.add(:start_time, "は土曜日を選択できません") if start_time.saturday?
+  end
+  
+  def time_only_1
+    if hour_only_1 && min_only
+      true
+    elsif hour_only_2 && min_only
+      true
+    else
+      errors.add(:start_time, "予約時間は13:15もしくは19:15になります")
+    end
+  end
+
+  def hour_only_1
+    start_time.hour == 19
+  end
+
+  def hour_only_2
+    start_time.hour == 13
+  end
+
+  def min_only
+    start_time.min == 15
   end
 end
