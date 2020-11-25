eab0c7dd5f39206da1cedfe0bc892ded046fa72d
kohei abe <k_913411@yahoo.co.jp>
2020-11-25 23:39:05 +0900

    add to factory data user for reservations

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,11 +1,12 @@
 class Reservation < ApplicationRecord
-  validates :name, presence: true
+  belongs_to :user
+  validates :user_id, presence: true
+  validates :name,    presence: true
   validate :date_before_start
   validate :start_time_not_sunday
   validate :start_time_not_saturday
   validate :time_only
   validates :start_time, uniqueness: { message: 'は他のユーザーが予約しています' }
-  belongs_to :user
   scope :future_reservations, -> do
     where("start_time > ?", Date.today).order(start_time: :desc)
   end
--- spec/factories/reservations.rb
+++ spec/factories/reservations.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :reservation do
     name { "MyString" }
     start_time { "2090-10-23 19:15" }
+    user
   end
 end
