0b90e92372c6808b491df5f07ae3c7bb3c6cb1d5
kohei abe <k_913411@yahoo.co.jp>
2020-11-29 20:40:07 +0900

    add to validation for reservation menu_id:culumn

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,7 @@ class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :menu
   validates :user_id, presence: true
+  validates :menu_id, presence: true
   validates :name,    presence: true
   validate :date_before_start
   validate :start_time_not_sunday
